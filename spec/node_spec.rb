47c8851407a828f4f1bf90c9f413e10f81c47dd6
Matt Levy <milevy1@gmail.com>
2019-04-24 15:37:47 -0600

    Added Node class with #data and #next_node instance methods

---
+++ spec/node_spec.rb
@@ -0,0 +1,27 @@
+require "./lib/node"
+
+RSpec.describe 'Jungle Beats Linked List' do
+  describe 'Node' do
+    before :each do
+      @node = Node.new("plop")
+    end
+    it 'can be initialized with data' do
+
+      expect(@node).to be_an_instance_of(Node)
+    end
+
+    describe 'instance methods' do
+      describe '#data' do
+        it 'returns the data held by the node' do
+          expect(@node.data).to eq("plop")
+        end
+      end
+
+      describe '#next_node' do
+        it 'returns nil if there is no next node' do
+          expect(@node.next_node).to be_nil
+        end
+      end
+    end
+  end
+end
