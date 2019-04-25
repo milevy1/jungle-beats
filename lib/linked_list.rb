a93376d2f91a5613193469d2db9b582dd6ba112e
Matt Levy <milevy1@gmail.com>
2019-04-25 13:06:07 -0600

    Refactored head attributes to use attr_reader

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,21 +6,21 @@ class LinkedList
   end
 
   def append(data)
-    if @head.nil?
+    if head.nil?
       @head = Node.new(data)
     else
-      @head.append_node(data)
+      head.append_node(data)
     end
     data
   end
 
   def prepend(data)
-    if @head.nil?
+    if head.nil?
       append(data)
     else
-      existing_list = @head
+      existing_list = head
       @head = Node.new(data)
-      @head.next_node = existing_list
+      head.next_node = existing_list
     end
   end
 
@@ -30,47 +30,47 @@ class LinkedList
     elsif position < 0           # Prepend if position is negative
       prepend(data)
     else
-      @head.insert_node(position, data)
+      head.insert_node(position, data)
     end
   end
 
   def count
-    if @head.nil?
+    if head.nil?
       0
     else
-      @head.count_nodes
+      head.count_nodes
     end
   end
 
   def to_string
-    if @head.nil?
+    if head.nil?
       nil
     else
-      @head.generate_string
+      head.generate_string
     end
   end
 
   def find(position, element_count)
-    if @head.nil?
+    if head.nil?
       nil
     else
-      @head.find_nodes(position, element_count)
+      head.find_nodes(position, element_count)
     end
   end
 
   def includes?(value)
-    if @head == value
+    if head == value
       true
     else
-      @head.includes?(value)
+      head.includes?(value)
     end
   end
 
   def pop
-    if @head.nil?
+    if head.nil?
       nil
     else
-      @head.pop
+      head.pop
     end
   end
 end
